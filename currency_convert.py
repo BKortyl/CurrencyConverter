d6d41a382559c737e57c380a9db1bfb1406e1d34
Bartosz Kortylewski <bartosz.kortylewski@gmail.com>
2021-04-14 00:14:16 +0200

    Add ability to input currency code and amount, returning how much USD and EUR you'd get from selling that

--- currency_convert.py
+++ currency_convert.py
@@ -1,7 +1,12 @@
 # work in progress
+import requests
 
-crypto_amount = float(input('Please, enter the number of crypto currency you have: '))
-currencies = {'RUB': 2.98, 'ARS': 0.82, 'HNL': 0.17, 'AUD': 1.9622, 'MAD': 0.208}
-for key in currencies:
-    foreign_amount = round(float(crypto_amount) * currencies[key], 2)
-    print(f'I will get {foreign_amount} {key} from the sale of {crypto_amount} conicoins.')
+
+currency_code = input('Please enter the currency code: ')
+currency_amount = float(input('Please, enter the number of currency you have: '))
+currencies = {1: 'usd', 2: 'eur'}
+r = requests.get(f'http://www.floatrates.com/daily/{currency_code.lower()}.json')
+data_json = r.json()
+for item in currencies:
+    foreign_amount = round(float(currency_amount) * data_json[currencies[item]]['rate'], 2)
+    print(f'I will get {foreign_amount} {currencies[item].upper()} from the sale of {currency_amount} {currency_code.upper()}.')
