dc8362fc36557b53dd4be433ec6a09de87004e37
Bartosz Kortylewski <bartosz.kortylewski@gmail.com>
2021-04-22 23:13:57 +0200

    Encompass existing code into calc(), add menu() to allow switching program modes.

--- currency_convert.py
+++ currency_convert.py
@@ -2,11 +2,28 @@
 import requests
 
 
-currency_code = input('Please enter the currency code: ')
-currency_amount = float(input('Please, enter the number of currency you have: '))
-currencies = {1: 'usd', 2: 'eur'}
-r = requests.get(f'http://www.floatrates.com/daily/{currency_code.lower()}.json')
-data_json = r.json()
-for item in currencies:
-    foreign_amount = round(float(currency_amount) * data_json[currencies[item]]['rate'], 2)
-    print(f'I will get {foreign_amount} {currencies[item].upper()} from the sale of {currency_amount} {currency_code.upper()}.')
+def menu():
+    while True:
+        choice = input("'calc' for calculator, 'list' for a list of current exchange rates: ")
+        if choice == 'calc':
+            calc()
+        elif choice == 'list':
+            pass # TODO: Parse data from NBP site, add into single dictionary, cache requests and allow multiples.
+        elif choice == 'quit':
+            quit()
+        else:
+            print('Wrong input')
+
+
+def calc():
+    currency_code = input('Please enter the currency code: ')
+    currency_amount = float(input('Please, enter the amount of currency you have: '))
+    currencies = {1: 'usd', 2: 'eur'}
+    r = requests.get(f'http://www.floatrates.com/daily/{currency_code.lower()}.json')
+    data_json = r.json()
+    for item in currencies:
+        foreign_amount = round(float(currency_amount) * data_json[currencies[item]]['rate'], 2)
+        print(f'I will get {foreign_amount} {currencies[item].upper()} from the sale of {currency_amount} {currency_code.upper()}.')
+
+
+menu()
