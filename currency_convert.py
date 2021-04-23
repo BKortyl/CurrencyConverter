a43c6ae8ccf8879ad0733df53a7790fd861d499b
Bartosz Kortylewski <bartosz.kortylewski@gmail.com>
2021-04-23 03:30:04 +0200

    Rework calc to work with NBP API, fix bug for lowercase input on exchange rates

--- currency_convert.py
+++ currency_convert.py
@@ -4,7 +4,7 @@ import requests
 
 def menu():
     while True:
-        choice = input("'calc' for calculator, 'list' for a list of current exchange rates: ")
+        choice = input("'calc' for calculator, 'list' for a list of current exchange rates, 'quit' to quit: ")
         if choice == 'calc':
             calculate_exchange()
         elif choice == 'list':
@@ -16,15 +16,16 @@ def menu():
             print('Wrong input')
 
 
-def calculate_exchange():  # TODO: Change to NBP API, rework to take currency which you want to get.
-    currency_code = input('Please enter the currency code: ')
+def calculate_exchange():  # TODO: Doesn't work for PLN, predict and take care of possible errors.
+    currency_in = input('Please enter the code of currency that you have: ')
     currency_amount = float(input('Please, enter the amount of currency you have: '))
-    currencies = {1: 'usd', 2: 'eur'}
-    r = requests.get(f'http://www.floatrates.com/daily/{currency_code.lower()}.json')
-    data_json = r.json()
-    for item in currencies:
-        foreign_amount = round(float(currency_amount) * data_json[currencies[item]]['rate'], 2)
-        print(f'I will get {foreign_amount} {currencies[item].upper()} from the sale of {currency_amount} {currency_code.upper()}.')
+    currency_out = input('Please enter the code of currency that you want to exchange to: ')
+    request_in = requests.get(f'http://api.nbp.pl/api/exchangerates/rates/a/{currency_in}/?format=json')
+    data_in = request_in.json()
+    request_out = requests.get(f'http://api.nbp.pl/api/exchangerates/rates/a/{currency_out}/?format=json')
+    data_out = request_out.json()
+    result = round(currency_amount * data_in['rates'][0]['mid'] / data_out['rates'][0]['mid'], 2)
+    print(f'I will get {result} {currency_out.upper()} from the sale of {currency_amount} {currency_in.upper()}.')
 
 
 def exchange_rates(cache):
@@ -37,8 +38,8 @@ def exchange_rates(cache):
             print('Current exchange rates:')
             for currency in flagged_currency:
                 print(f'1 {currency} : {round(flagged_currency[currency], 2)} PLN')
-        elif currency_code in cache:
-            flagged_currency[currency_code] = cache[currency_code]
+        elif currency_code.upper() in cache:
+            flagged_currency[currency_code.upper()] = cache[currency_code.upper()]
         else:
             print('Wrong input')
 
